963fd5b35f5826d1695de5f7245685efed392d72
radha540 <rbalu2004@gmail.com>
2022-09-10 16:24:33 +0530

    project sturture

--- src/test/java/org/day1/Projecttask1.java
+++ src/test/java/org/day1/Projecttask1.java
@@ -2,6 +2,8 @@ package org.day1;
 
 public class Projecttask1 {
 public static void main(String[] args) {
-	
+	System.out.println("test1");
+	System.out.println("test2");
+	System.out.println("test3");
 }
 }
